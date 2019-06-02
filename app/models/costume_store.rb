cfe525fb72f1ade3b819aa56a07a35e69735b5f7
Kat B <katreesebarnes@gmail.com>
2019-06-02 00:27:28 +0000

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,7 @@
+class CostumeStore < ActiveRecord::Base
+end
+
+
+
+
 # Create your CostumeStore class here
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,11 @@
+class CreateHauntedHouse < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |t|
+     t.string :name
+  end 
+end
+
+
+
+
 # Create your haunted_houses migration here
